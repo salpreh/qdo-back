ff771cb2110cc8c1b0003a208ba67b17d9744562
Salva Pérez <sperez@ticnux.com>
2020-07-20 12:52:18 +0200

    Initialzr app first commit

---
+++ src/main/java/com/salpreh/qdo/QdoApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.qdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salpreh/qdo/QdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salpreh.qdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
